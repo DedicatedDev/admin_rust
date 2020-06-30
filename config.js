71df9aeae78befc674e29e4538ccfa79072c2a0e
updev0612 <55649280+updev0612@users.noreply.github.com>
2020-06-30 04:07:15 +0300

    mealzCard Json Upload to firbase

---
+++ config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDh9oN7e4ElQmU2eRPB1Dp1KREc-5amaE4",
+    authDomain: "mealzcard-872c4.firebaseapp.com",
+    databaseURL: "https://mealzcard-872c4.firebaseio.com",
+    projectId: "mealzcard-872c4",
+    storageBucket: "mealzcard-872c4.appspot.com",
+    messagingSenderId: "949907564",
+    appId: "1:949907564:web:5fecd6fd844b4670ffa849",
+    measurementId: "G-78X139T8LW"
+  };
+  // Exports
+  module.exports = firebaseConfig;
